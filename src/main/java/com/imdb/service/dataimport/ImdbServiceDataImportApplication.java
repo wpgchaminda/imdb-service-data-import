19590b32f270d5f1c127629169a7f29659e4d8c3
Chaminda Jayawardana <wpgchaminda@gmail.com>
2022-07-17 11:05:06 +0530

    Project Init

---
+++ src/main/java/com/imdb/service/dataimport/ImdbServiceDataImportApplication.java
@@ -0,0 +1,13 @@
+package com.imdb.service.dataimport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbServiceDataImportApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ImdbServiceDataImportApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/imdb/service/dataimport/ImdbServiceDataImportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imdb.service.dataimport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdbServiceDataImportApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
